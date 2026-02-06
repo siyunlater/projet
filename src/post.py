6a23ba2c77fa363cfdaf4af0687fba9fae774c0e
siyunlater@gmail.com <siyunlater@gmail.com>
2026-02-06 12:44:52 +0100

    improving 2D distribution profile of fission rate and heating, add 1D profile of fission rate

--- src/post.py
+++ src/post.py
@@ -17,17 +17,12 @@ sp = openmc.StatePoint(SP_FILE)
 fission = sp.get_tally(scores=['fission'])
 heating = sp.get_tally(scores=['heating'])
 
-fission.std_dev.shape = (100, 100)
-fission.mean.shape = (100, 100)
-heating.std_dev.shape = (100, 100)
-heating.mean.shape = (100, 100)
-
-fig = plt.subplot(121)
-fig.imshow(fission.mean)
-fig2 = plt.subplot(122)
-fig2.imshow(heating.mean)
-plt.savefig('figures.png', dpi=300)
-
+# Data Reshape
+# mesh.dimension = [100, 100]
+f_mean = fission.mean.reshape((100, 100))
+f_std  = fission.std_dev.reshape((100, 100))
+h_mean = heating.mean.reshape((100, 100))
+h_std  = heating.std_dev.reshape((100, 100))
 
 # Pandas data frame
 df_fission = fission.get_pandas_dataframe(nuclides=False)
@@ -38,24 +33,42 @@ pd.options.display.float_format = '{:.2e}'.format
 df = pd.concat([df_fission, df_heating], ignore_index=True)
 df.to_csv('data_fission_and_heating.csv', index=False)
 
-## =================================================================
-## 3. Graph 
-## =================================================================
-#
-#fission.mean.shape = (100, 100)
-#fission.std_dev.shape = (100, 100)
-#heating.mean.shape = (100, 100)
-#heating.std_dev.shape = (100, 100)
-#
-#fig = plt.subplot(121)
-#fig.imshow(fission.mean)
-#fig2 = plt.subplot(122)
-#fig2.imshow(heating.mean)
-#
-## Determine relative error
-#relative_error = np.zeros_like(fission.std_dev)
-#nonzero = fission.mean > 0
-#relative_error[nonzero] = fission.std_dev / fission.mean
-#
-## distribution of relative errors
-#ret = plt.hist(relative_error[nonzero], bins=50)
+# =================================================================
+# 3. Graph 
+# =================================================================
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+# Fission Rate Distribution (2D)
+im1 = ax1.imshow(f_mean, cmap='viridis', origin='lower', interpolation='gaussian')
+ax1.set_title("Fission Rate Distribution")
+plt.colorbar(im1, ax=ax1, label='Fissions / source particle')
+
+# Heating (Energy Deposition) Distribution (2D)
+im2 = ax2.imshow(h_mean, cmap='magma', origin='lower', interpolation='gaussian')
+ax2.set_title("Energy Deposition (Heating)")
+plt.colorbar(im2, ax=ax2, label='eV / source particle')
+
+plt.tight_layout()
+plt.savefig('distribution.png', dpi=300)
+
+# Radial profile (1D)
+center_idx = 20 # 100/2
+radial_fission = fission.mean[center_idx, :]
+
+plt.figure()
+x = np.arange(100)
+y_fission = f_mean[center_idx, :].flatten()
+y_error = f_std[center_idx, :].flatten()
+
+plt.plot(x, y_fission, label='Fission Rate', color='blue')
+plt.fill_between(x, 
+                 y_fission - y_error, 
+                 y_fission + y_error, 
+                 alpha=0.3, color='blue', label='Stat. Uncertainty')
+
+plt.xlabel('Mesh Index (X-direction)')
+plt.ylabel('Reaction Rate')
+plt.title('Radial Fission Rate Profile (at center Y)')
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.legend()
+plt.savefig('radial_profile_1d.png', dpi=300)
